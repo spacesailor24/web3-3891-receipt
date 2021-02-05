f13cb26adb6c58dc9c334d5583608e33e5ac4a75
Wyatt Barnes <me@wyatt.email>
2021-02-05 11:12:51 -0600

    Add receipt listener

--- index.ts
+++ index.ts
@@ -8,16 +8,41 @@ const ganacheAddress1 = '0xc96a4110AA93879608756A9d22a88614634BD564';
 (async () => {
     console.log(`SANITY CHECK should be true: ${await web3.eth.getBalance(ganacheAddress1) !== '0'}`)
 
+    let deployedAddress
+    let contract
+
     // @ts-ignore
-    const contract = new web3.eth.Contract(SimpleStorage.abi)
-    contract.deploy({data: SimpleStorage.bytecode})
+    contract = new web3.eth.Contract(SimpleStorage.abi)
+    await contract.deploy({data: SimpleStorage.bytecode})
         .send({
             from: ganacheAddress1,
             gas: 1500000,
             gasPrice: '30000000000000'
         }, (err, txHash) => {
             console.log(`TxHash: ${txHash}`)
-        }).then(deployed => {
-            console.log(`Deployed contract address: ${deployed.options.address}`)
         })
+        .on('receipt', (receipt) => {
+            console.log(`Tx Receipt: ${JSON.stringify(receipt, null, 4)}`)
+        })
+        .then(deployed => {
+            console.log(`Deployed contract address: ${deployed.options.address}`)
+            deployedAddress = deployed.options.address
+        })
+
+    // @ts-ignore
+    contract = new web3.eth.Contract(SimpleStorage.abi, deployedAddress)
+    contract.methods.set(4)
+    .send({
+        from: ganacheAddress1,
+        gas: 1500000,
+        gasPrice: '30000000000000'
+    }, (err: any, txHash: any) => {
+        console.log(`TxHash: ${txHash}`)
+    })
+    .on('receipt', (receipt: any) => {
+        console.log(`Tx Receipt: ${JSON.stringify(receipt, null, 4)}`)
+    })
+    .then((receipt: any) => {
+        console.log(`Tx Receipt 2: ${JSON.stringify(receipt, null, 4)}`)
+    })
 })()
